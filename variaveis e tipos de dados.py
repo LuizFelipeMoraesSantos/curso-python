e16763966475a3698fdbd1e6c5bf4f03c15c5a8c
Marcelo-Rocha00 <marcelorochafilho04@gmail.com>
2024-09-25 19:06:46 -0300

    variaveis e tipos de dados
    feito durante a aula com alguns tipos de variaveis e dados

---
+++ variaveis e tipos de dados.py
@@ -0,0 +1,35 @@
+# segunda aula
+#sintaxe basica de python
+
+#numeros
+x = 10 #tipo inteiro
+
+y = 3.14 #tipo float
+
+z = 1+2j #operação matematica
+
+#seguencia de caracteres
+
+string = "ola mundo"
+lista = [1,2,3,4,5]
+tupla = (1,2,3,4,5)
+
+#nome variavel
+
+primeiro_numero = 10 #snake casel(todos os caracteres minusculos)
+Segundo_Numero = 20 #camel case(A primeira letra da variavel sempre maiuscula)
+tercerironumemo = 30 #todos os caracteres juntos
+
+#dicionario
+
+dic = {'nome':'jhon', 'idade': 30, 'cidade': 'Nova York'}
+
+#booleans
+
+Verdadeiro = True
+Falso = False
+
+#outros
+
+nulo = None
+tipo_objeto = type ('colocar o nome da variável para descobrir seu tipo')
