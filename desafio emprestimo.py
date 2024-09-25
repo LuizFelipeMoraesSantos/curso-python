14a8da48dff2192959b942a1411ef78dfde15462
Marcelo-Rocha00 <marcelorochafilho04@gmail.com>
2024-09-25 20:38:15 -0300

    desafios
    desafios propostos pelo professor 1 utilizando porcentagem com o if é o outro so utilizando if, elif e else

---
+++ desafio emprestimo.py
@@ -0,0 +1,11 @@
+salario = float(input("insira seu salario para avaliarmos o emprestimo: "))
+emprestimo = float(input("insira a quantidade que deseja pegar no emprestimo: "))
+
+if (emprestimo <= salario*0.5):
+    print("emprestimo aprovado")
+    
+elif (emprestimo <= salario*0.75):
+    print("emprestimo em analise")
+    
+else:
+    print("emprestimo não aprovado")
---
+++ desconto.py
@@ -0,0 +1,11 @@
+cupom = input("Digite o cupom: ")
+
+if (cupom == "FUCTURA1"):
+    print("você ganhou 15% de desconto")
+    
+elif (cupom == "FUCTURA2"):
+    print("você ganhou 10% de desconto")
+    
+else:
+    print("você ganhou 5% de desconto")
+
