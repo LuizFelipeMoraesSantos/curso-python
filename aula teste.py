02d84702e6f1e9803297e747492fb2494ef6b4e2
Marcelo-Rocha00 <marcelorochafilho04@gmail.com>
2024-09-24 21:20:59 -0300

    att

---
+++ aula teste.py
@@ -0,0 +1,10 @@
+#Entrada de dados
+
+salario_Base = float(input("Salario base: "))
+gratificação = float(input("Gratificação: "))
+
+#processamento
+salarioBruto = salario_Base + gratificação
+
+#saida
+print(f"O valor do sálario bruto é R${salarioBruto}")
