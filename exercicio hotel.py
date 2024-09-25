aaa6309b3e9f576ccad7e3497509c932567c849f
Marcelo-Rocha00 <marcelorochafilho04@gmail.com>
2024-09-25 19:05:46 -0300

    exercicio cartão de hotel
    exercício feito em sala de aula que pediu para fazer um cartão de hotel, utilizando input e print

---
+++ exercicio hotel.py
@@ -0,0 +1,11 @@
+hotel = input("Insira o nome do hotel (até 6 a 8 caracteres): ")
+estrelas = input("insira a quantidade de estrelas que o hotel recebe (ate 5 estrelas): ")
+cidade = input("Insira o nome da cidade: ")
+
+
+
+print("*"*20)
+print("*"*7, f"{hotel}", "*"*7) 
+print("*"*4, f"{estrelas} estrelas", "*"*4)
+print("*"*4, f"Em {cidade} ", "*"*4)
+print("*"*20)
