9a49ed73ae63b76ba3835feaf54cabaa47ebc0ca
Marcelo-Rocha00 <marcelorochafilho04@gmail.com>
2024-09-27 21:07:47 -0300

    Create exercicio_Strings.py
    exercicios de strings com oque foi aprendido na sala

---
+++ exercicio_Strings.py
@@ -0,0 +1,14 @@
+frase = "Estruturas de Dados em Python"
+
+#deixando a string inteira maiuscula
+maiuscula = frase.upper()
+print(maiuscula)
+
+encontrar = frase.find("Dados")
+print(encontrar)
+
+trocar = frase.replace("Python", "Java")
+print(trocar)
+
+dividir = frase.split()
+print(dividir)
