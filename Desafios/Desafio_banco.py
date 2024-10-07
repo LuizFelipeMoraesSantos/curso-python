bc6f60abf714c04c62cf3c4857eebbe967172512
Marcelo-Rocha00 <marcelorochafilho04@gmail.com>
2024-10-07 19:47:58 -0300

    Update Desafio_banco.py

--- Desafios/Desafio_banco.py
+++ Desafios/Desafio_banco.py
@@ -20,13 +20,15 @@ while True:
         
     elif opcao == "2": #se a opção for '2', verificar o saldo da conta
         def Verificar_saldo():
-           nome_user = input("insira seu nome para verificar seu saldo: ")# solicitando o nome de usuário para acessar o dicionario que guarda as informções do usuario.
-
-           if nome_user in informações_cliente: #se o nome de usuario existir em 'informações_cliente'. seguir para o codigo abaixo
-                saldo = informações_cliente[nome_user]["Saldo"] #acessando o dicionario pela chave principal que é (nome_user) é chamando a chave que guarda o valor de saldo
-                print(f"O saldo do cliente é: R${saldo:.2f}") #imprimindo o valor atual de saldo
-
-           else:# caso o nome de usuario não existir em 'informações_cliente'. vai imprimir a mensagem abaixo.
+            nome_user = input("insira seu nome para verificar seu saldo: ")# solicitando o nome de usuário para acessar o dicionario que guarda as informções do usuario.
+            senha_user = input("insira sua senha: ")
+            if nome_user in informações_cliente: #se o nome de usuario existir em 'informações_cliente'. seguir para o codigo abaixo
+                if informações_cliente[nome_user]["Senha"] == senha_user: #verificando se senha do usuario esta correta
+                    saldo = informações_cliente[nome_user]["Saldo"] #acessando o dicionario pela chave principal que é (nome_user) é chamando a chave que guarda o valor de saldo
+                    print(f"O saldo do cliente é: R${saldo:.2f}") #imprimindo o valor atual de saldo
+                else:
+                    print("senha incorreta!! tente novamente")
+            else:# caso o nome de usuario não existir em 'informações_cliente'. vai imprimir a mensagem abaixo.
                 print("Conta de usuario não existe.")
                 
         Verificar_saldo() #chamando a função
@@ -34,12 +36,15 @@ while True:
     elif opcao == "3":#se a opção for '3', Depositar dinheiro
         def Depositar_dinheiro():
             nome_user = input("insira seu nome para depositar o saldo: ")# solicitando o nome de usuário para acessar o dicionario que guarda as informções do usuario.
-            
+            senha_user = input("insira sua senha: ")
             if nome_user in informações_cliente:#se o nome de usuario existir em 'informações_cliente'. seguir para o codigo abaixo
-                depositar = float(input("Insira o valor que deseja depositar: "))#pedindo para o usuario inserir o valor para executar o deposito
-                informações_cliente[nome_user]["Saldo"] += depositar #acessando a chave 'saldo' do dicionario é somando ela com o valor que o cliente inseriu em deposito
-                print(f"O deposito de {depositar:.2f} foi feito com sucesso.")#mensagem mostrando que o deposito foi bem sucedido
-
+                if informações_cliente[nome_user]["Senha"] == senha_user:
+                    depositar = float(input("Insira o valor que deseja depositar: "))#pedindo para o usuario inserir o valor para executar o deposito
+                    informações_cliente[nome_user]["Saldo"] += depositar #acessando a chave 'saldo' do dicionario é somando ela com o valor que o cliente inseriu em deposito
+                    print(f"O deposito de {depositar:.2f} foi feito com sucesso.")#mensagem mostrando que o deposito foi bem sucedido
+                
+                else:
+                    print("senha incorreta!! tente novamente")
             else:# caso o nome de usuario não existir em 'informações_cliente'. vai imprimir a mensagem abaixo.
                 print("Conta de usuario não existe.")
 
@@ -48,20 +53,24 @@ while True:
     elif opcao == "4":#se a opção for '4', sacar o dinheiro
         def sacar_dinheiro():
             nome_user = input("insira seu nome para Sacar o seu saldo: ")# solicitando o nome de usuário para acessar o dicionario que guarda as informções do usuario.
-
+            senha_user = input("insira sua senha: ")
             if nome_user in informações_cliente:#se o nome de usuario existir em 'informações_cliente'. seguir para o codigo abaixo
-                saldo = informações_cliente[nome_user]["Saldo"] #acessando a chave 'Saldo' para mostrar ao usuario o saldo disponivel
-                print(f"Saldo atual: {saldo}")#imprimindo o saldo atual
-                sacar = float(input("insira o valo que deseja Sacar: "))#pedindo para o usuario inserir qual o valor que deseja sacar
+                if informações_cliente[nome_user]["Senha"] == senha_user:
+                    saldo = informações_cliente[nome_user]["Saldo"] #acessando a chave 'Saldo' para mostrar ao usuario o saldo disponivel
+                    print(f"Saldo atual: {saldo:.2f}")#imprimindo o saldo atual
+                    sacar = float(input("insira o valo que deseja Sacar: "))#pedindo para o usuario inserir qual o valor que deseja sacar
+                
+                    
+                    if sacar > saldo: #se o valor do saque seja maior que o saldo da conta.imprime a mensagem abaixo
+                        print("Impossível fazer o saque. Saldo insuficiente.")
 
-                if sacar > saldo: #se o valor do saque seja maior que o saldo da conta.imprime a mensagem abaixo
-                    print("Impossível fazer o saque. Saldo insuficiente.")
+                    else:#caso o valor do saque seja menor ou igual ao do saldo ele segue o codigo.
+                        informações_cliente[nome_user]["Saldo"] -= sacar#acessando a chave 'Saldo' do dicionario é subtraindo com o valor inserindo no saque
+                        print(f"O saque de: {sacar:.2f} da conta de {nome_user} foi feito com sucesso.") # mensagem mostrando que o saque foi um sucesso
+                        print(f"Saldo Atual: R${informações_cliente[nome_user]["Saldo"]:.2f}")#imprimindo o saldo atual da conta
 
-                else:#caso o valor do saque seja menor ou igual ao do saldo ele segue o codigo.
-                    informações_cliente[nome_user]["Saldo"] -= sacar#acessando a chave 'Saldo' do dicionario é subtraindo com o valor inserindo no saque
-                    print(f"O saque de: {sacar:.2f} da conta de {nome_user} foi feito com sucesso.") # mensagem mostrando que o saque foi um sucesso
-                    print(f"Saldo Atual: R${informações_cliente[nome_user]["Saldo"]:.2f}")#imprimindo o saldo atual da conta
-
+                else:
+                    print("senha incorreta!! tente novamente")
             else:# caso o nome de usuario não existir em 'informações_cliente'. vai imprimir a mensagem abaixo.
                 print("conta de usuario não existe.")
         sacar_dinheiro()#chamando a função
