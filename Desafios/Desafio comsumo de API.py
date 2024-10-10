c9e4cc4f6118d74e70f354f282e3ca5beffe079a
Marcelo-Rocha00 <marcelorochafilho04@gmail.com>
2024-10-10 19:01:55 -0300

    att
    converti a API em um excel

--- Desafios/Desafio comsumo de API.py
+++ Desafios/Desafio comsumo de API.py
@@ -23,11 +23,48 @@ if response.status_code == 200:
     
     #adicionando dados da API ao Excel
     for user in users:
-        sheet.append([user['loteria'],user['concurso'],user['data'], user['local'],user['dezenasOrdemSorteio'],user['dezenas'],user['trevos'],user['timeCoracao'],user['mesSorte'],user['premiacoes']['descricao'],user['premiacoes']['faixa'],user['local'][ganhadores],user['local'][valorPremio],user['local']user['local']user['local']user['local']user['local']user['local']user['local']user['local']])
+        try:
+            dezenasOrdemSorteio = ', '.join(user.get('dezenasOrdemSorteio', []))
+            dezenas = ', '.join(user.get('dezenas', []))
+            trevos = ', '.join(user.get('trevos',[]))
+            
+            estadosPremiados = user.get('estadosPremiados', [])
+            estadosPremiados_str = ', '.join([str(i['local']) for i in  estadosPremiados if 'local' in i])
+            
+            localGanhadores = user.get('localGanhadores', [])
+            localGanhadores_str = ', '.join([str(g['local']) for g in localGanhadores if 'local' in g])
+            sheet.append([
+                user['loteria'],
+                user['concurso'],
+                user['data'],
+                user['local'],
+                dezenasOrdemSorteio,
+                dezenas,
+                trevos,
+                user['timeCoracao'],
+                user['mesSorte'],
+                user['premiacoes'][0]['descricao'],
+                user['premiacoes'][0]['faixa'],
+                user['premiacoes'][0]['ganhadores'],
+                user['premiacoes'][0]["valorPremio"],
+                estadosPremiados_str,
+                user['observacao'],
+                user['acumulou'],
+                user['proximoConcurso'],
+                user['dataProximoConcurso'],
+                localGanhadores_str,
+                user['valorArrecadado'],
+                user['valorAcumuladoConcurso_0_5'],
+                user['valorAcumuladoConcursoEspecial'],
+                user['valorAcumuladoProximoConcurso'],
+                user['valorEstimadoProximoConcurso']
+            ])
+        except KeyError as e:
+            print(f"Chave ausente em user: {e}")
         
     #salvando o arquivo
     workbook.save("Mega_Sena.xlsx")
-    print("Dados salvos no arquivo 'usuarios_api.xlsx' com sucesso.")
+    print("Dados salvos no arquivo 'Mega_Sena.xlsx' com sucesso.")
     
 else:
     print(f"falha ao obter dados. status code: {response.status_code}")
